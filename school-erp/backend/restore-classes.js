04361e8121b39be603d320ee2c6daf51cd03f35e
Niraj Karnawat <karnawatniraj@gmail.com>
2026-05-01 20:08:43 +0530

    security: remove .env from tracking, add to .gitignore, fix hardcoded MongoDB URIs

--- school-erp/backend/restore-classes.js
+++ school-erp/backend/restore-classes.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 async function fixClasses() {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/school_erp');
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
     
     const db = mongoose.connection.db;
